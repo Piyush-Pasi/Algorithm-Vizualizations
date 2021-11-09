02108d2841329d63b8d040a621a98c10e7ba47ed
Piyush-Pasi <amazingpiyush4@gmail.com>
2021-11-10 00:02:34 +0530

    adding app and cmds

--- index.py
+++ index.py
@@ -119,3 +119,7 @@ def input_graph():
 
     '''
     return render_template('inputGraph.html')
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
